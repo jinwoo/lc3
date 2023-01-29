4619c38964d2e17a1f40cee450bb284b7dee7db1
Jinwoo Lee <jinwoo@google.com>
2023-01-29 10:36:31 -0800

    define type vm

--- lc3.ml
+++ lc3.ml
@@ -2,33 +2,8 @@
 let mr_kbsr = 0xfe00 (* keyboard status *)
 let mr_kbdr = 0xfe02 (* keyboard data *)
 
-(* Trap codes *)
-type trapcode =
-  (* Get character from keyboard, not echoed onto the terminal. *)
-  | Trap_getc
-  (* Output a character. *)
-  | Trap_out
-  (* Output a word string. *)
-  | Trap_puts
-  (* Get character from keyboard, echoed onto the terminal. *)
-  | Trap_in
-  (* Output a byte string. *)
-  | Trap_putsp
-  (* Halt the program. *)
-  | Trap_halt
-
-let trapcode_of_int = function
-  | 0x20 -> Trap_getc
-  | 0x21 -> Trap_out
-  | 0x22 -> Trap_puts
-  | 0x23 -> Trap_in
-  | 0x24 -> Trap_putsp
-  | 0x25 -> Trap_halt
-  | n -> failwith ("invalid trapcode: " ^ string_of_int n)
-
 (* Memory *)
 let memory_max = 1 lsl 16
-let memory = Array.make memory_max 0
 
 (* Registers *)
 let r_r0 = 0
@@ -36,7 +11,23 @@ let r_r7 = 7
 let r_pc = 8
 let r_cond = 9
 let r_count = 10
-let reg = Array.make r_count 0
+
+(* Condition flags *)
+let fl_pos = 1 lsl 0
+let fl_zro = 1 lsl 1
+let fl_neg = 1 lsl 2
+
+type vm = { memory : int array; reg : int array; mutable running : bool }
+
+let make_vm () =
+  let memory = Array.make memory_max 0 in
+  let reg = Array.make r_count 0 in
+  (* Since exactly one condition flag should be set at any given time,
+     set the Z flag. *)
+  reg.(r_cond) <- fl_zro;
+  (* Set the PC to starting position. 0x3000 is the default. *)
+  reg.(r_pc) <- 0x3000;
+  { memory; reg; running = true }
 
 type opcode =
   | Op_br (* branch *)
@@ -75,12 +66,31 @@ let opcode_of_int = function
   | 15 -> Op_trap
   | n -> invalid_arg ("invalid opcode " ^ string_of_int n)
 
-(* Condition flags *)
-let fl_pos = 1 lsl 0
-let fl_zro = 1 lsl 1
-let fl_neg = 1 lsl 2
+(* Trap codes *)
+type trapcode =
+  (* Get character from keyboard, not echoed onto the terminal. *)
+  | Trap_getc
+  (* Output a character. *)
+  | Trap_out
+  (* Output a word string. *)
+  | Trap_puts
+  (* Get character from keyboard, echoed onto the terminal. *)
+  | Trap_in
+  (* Output a byte string. *)
+  | Trap_putsp
+  (* Halt the program. *)
+  | Trap_halt
 
-let read_image image_path =
+let trapcode_of_int = function
+  | 0x20 -> Trap_getc
+  | 0x21 -> Trap_out
+  | 0x22 -> Trap_puts
+  | 0x23 -> Trap_in
+  | 0x24 -> Trap_putsp
+  | 0x25 -> Trap_halt
+  | n -> failwith ("invalid trapcode: " ^ string_of_int n)
+
+let read_image { memory; _ } image_path =
   In_channel.with_open_bin image_path (fun ic ->
       let buf = Bytes.create 2 in
       match In_channel.really_input ic buf 0 2 with
@@ -99,15 +109,6 @@ let read_image image_path =
           in
           aux 0)
 
-let load_arguments () =
-  let argc = Array.length Sys.argv in
-  if argc < 2 then (
-    print_endline "lc3 [image-file1] ...";
-    exit 2);
-  for i = 1 to argc - 1 do
-    read_image Sys.argv.(i)
-  done
-
 let original_tio = Unix.tcgetattr Unix.stdin
 
 let disable_input_buffering () =
@@ -120,7 +121,9 @@ let restore_input_buffering () =
   tcsetattr stdin TCSANOW original_tio
 
 let check_key () =
-  match Unix.select [ Unix.stdin ] [] [] 0. with [], _, _ -> false | _ -> true
+  match Unix.select [ Unix.stdin ] [] [] 0. with
+  | [], _, _ -> false
+  | _ :: _, _, _ -> true
 
 let handle_interrupt _ =
   restore_input_buffering ();
@@ -131,7 +134,7 @@ let setup () =
   Sys.set_signal Sys.sigint (Sys.Signal_handle handle_interrupt);
   disable_input_buffering ()
 
-let mem_read address =
+let mem_read { memory; _ } address =
   if address = mr_kbsr then
     if check_key () then (
       memory.(mr_kbsr) <- 1 lsl 15;
@@ -139,14 +142,13 @@ let mem_read address =
     else memory.(mr_kbsr) <- 0;
   memory.(address)
 
-let mem_write address value = memory.(address) <- value
-let running = ref true
+let mem_write { memory; _ } address value = memory.(address) <- value
 
 let sign_extend x bit_count =
   if (x lsr (bit_count - 1)) land 1 <> 0 then x lor (0xffff lsl bit_count)
   else x
 
-let update_flags r =
+let update_flags { reg; _ } r =
   let fl =
     if reg.(r) = 0 then fl_zro
     else if
@@ -156,161 +158,199 @@ let update_flags r =
   in
   reg.(r_cond) <- fl
 
-let rec run () =
-  if not !running then ()
-  else (
-    (let pc = reg.(r_pc) in
-     reg.(r_pc) <- pc + 1;
-     let instr = mem_read pc in
-     match instr lsr 12 |> opcode_of_int with
-     | Op_add ->
-         (* Destination register (DR) *)
-         let r0 = (instr lsr 9) land 0x7 in
-         (* First operand (SR1) *)
-         let r1 = (instr lsr 6) land 0x7 in
-         (* Whether we are in immediate mode *)
-         let imm_flag = (instr lsr 5) land 0x1 in
-         (if imm_flag <> 0 then
-          let imm5 = sign_extend (instr land 0x1f) 5 in
-          reg.(r0) <- (reg.(r1) + imm5) land 0xffff
-         else
-           let r2 = instr land 0x7 in
-           reg.(r0) <- (reg.(r1) + reg.(r2)) land 0xffff);
-         update_flags r0
-     | Op_and ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let r1 = (instr lsr 6) land 0x7 in
-         let imm_flag = (instr lsr 5) land 0x1 in
-         (if imm_flag <> 0 then
-          let imm5 = sign_extend (instr land 0x1f) 5 in
-          reg.(r0) <- reg.(r1) land imm5
-         else
-           let r2 = instr land 0x7 in
-           reg.(r0) <- reg.(r1) land reg.(r2));
-         update_flags r0
-     | Op_not ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let r1 = (instr lsr 6) land 0x7 in
-         reg.(r0) <- lnot reg.(r1) land 0xffff;
-         update_flags r0
-     | Op_br ->
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         let cond_flag = (instr lsr 9) land 0x7 in
-         if cond_flag land reg.(r_cond) <> 0 then
-           reg.(r_pc) <- (reg.(r_pc) + pc_offset) land 0xffff
-     | Op_jmp ->
-         let r1 = (instr lsr 6) land 0x7 in
-         reg.(r_pc) <- reg.(r1)
-     | Op_jsr ->
-         let long_flag = (instr lsr 11) land 1 in
-         reg.(r_r7) <- reg.(r_pc);
-         if long_flag <> 0 then
-           (* JSR *)
-           let long_pc_offset = sign_extend (instr land 0x7ff) 11 in
-           reg.(r_pc) <- (reg.(r_pc) + long_pc_offset) land 0xffff
-         else
-           (* JSRR *)
-           let r1 = (instr lsr 6) land 0x7 in
-           reg.(r_pc) <- reg.(r1)
-     | Op_ld ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         reg.(r0) <- mem_read ((reg.(r_pc) + pc_offset) land 0xffff);
-         update_flags r0
-     | Op_ldi ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         reg.(r0) <- mem_read (mem_read ((reg.(r_pc) + pc_offset) land 0xffff));
-         update_flags r0
-     | Op_ldr ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let r1 = (instr lsr 6) land 0x7 in
-         let offset = sign_extend (instr land 0x3f) 6 in
-         reg.(r0) <- mem_read ((reg.(r1) + offset) land 0xffff);
-         update_flags r0
-     | Op_lea ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         reg.(r0) <- (reg.(r_pc) + pc_offset) land 0xffff;
-         update_flags r0
-     | Op_st ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         mem_write ((reg.(r_pc) + pc_offset) land 0xffff) reg.(r0)
-     | Op_sti ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let pc_offset = sign_extend (instr land 0x1ff) 9 in
-         mem_write (mem_read ((reg.(r_pc) + pc_offset) land 0xffff)) reg.(r0)
-     | Op_str ->
-         let r0 = (instr lsr 9) land 0x7 in
-         let r1 = (instr lsr 6) land 0x7 in
-         let offset = sign_extend (instr land 0x3f) 6 in
-         mem_write ((reg.(r1) + offset) land 0xffff) reg.(r0)
-     | Op_trap -> (
-         reg.(r_r7) <- reg.(r_pc);
-         match instr land 0xff |> trapcode_of_int with
-         | Trap_getc ->
-             reg.(r_r0) <- input_char stdin |> int_of_char;
-             update_flags r_r0
-         | Trap_out ->
-             output_char stdout (reg.(r_r0) |> char_of_int);
-             flush stdout
-         | Trap_puts ->
-             let rec aux i =
-               let c = memory.(reg.(r_r0) + i) in
-               if c = 0 then ()
-               else (
-                 output_char stdout (char_of_int c);
-                 aux (i + 1))
-             in
-             aux 0;
-             flush stdout
-         | Trap_in ->
-             print_string "Enter a character: ";
-             flush stdout;
-             let c = input_char stdin in
-             output_char stdout c;
-             flush stdout;
-             reg.(r_r0) <- int_of_char c;
-             update_flags r_r0
-         | Trap_putsp ->
-             let rec aux i =
-               let c = memory.(reg.(r_r0) + i) in
-               if c = 0 then ()
-               else
-                 let char1 = c land 0xff in
-                 if char1 = 0 then ()
-                 else (
-                   output_char stdout (char_of_int char1);
-                   let char2 = c lsr 8 in
-                   if char2 = 0 then ()
-                   else (
-                     output_char stdout (char_of_int char2);
-                     aux (i + 1)))
-             in
-             aux 0;
-             flush stdout
-         | Trap_halt ->
-             print_endline "HALT";
-             flush stdout;
-             running := false)
-     | Op_rti -> failwith "RTI"
-     | Op_res -> failwith "RES");
-    run ())
+let exec_add ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let r1 = (instr lsr 6) land 0x7 in
+  let imm_flag = (instr lsr 5) land 0x1 in
+  (if imm_flag <> 0 then
+   let imm5 = sign_extend (instr land 0x1f) 5 in
+   reg.(r0) <- (reg.(r1) + imm5) land 0xffff
+  else
+    let r2 = instr land 0x7 in
+    reg.(r0) <- (reg.(r1) + reg.(r2)) land 0xffff);
+  update_flags vm r0
 
-let main () =
-  load_arguments ();
+let exec_and ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let r1 = (instr lsr 6) land 0x7 in
+  let imm_flag = (instr lsr 5) land 0x1 in
+  (if imm_flag <> 0 then
+   let imm5 = sign_extend (instr land 0x1f) 5 in
+   reg.(r0) <- reg.(r1) land imm5
+  else
+    let r2 = instr land 0x7 in
+    reg.(r0) <- reg.(r1) land reg.(r2));
+  update_flags vm r0
+
+let exec_not ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let r1 = (instr lsr 6) land 0x7 in
+  reg.(r0) <- lnot reg.(r1) land 0xffff;
+  update_flags vm r0
+
+let exec_br { reg; _ } instr =
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  let cond_flag = (instr lsr 9) land 0x7 in
+  if cond_flag land reg.(r_cond) <> 0 then
+    reg.(r_pc) <- (reg.(r_pc) + pc_offset) land 0xffff
+
+let exec_jmp { reg; _ } instr =
+  let r1 = (instr lsr 6) land 0x7 in
+  reg.(r_pc) <- reg.(r1)
+
+let exec_jsr { reg; _ } instr =
+  let long_flag = (instr lsr 11) land 1 in
+  reg.(r_r7) <- reg.(r_pc);
+  if long_flag <> 0 then
+    (* JSR *)
+    let long_pc_offset = sign_extend (instr land 0x7ff) 11 in
+    reg.(r_pc) <- (reg.(r_pc) + long_pc_offset) land 0xffff
+  else
+    (* JSRR *)
+    let r1 = (instr lsr 6) land 0x7 in
+    reg.(r_pc) <- reg.(r1)
+
+let exec_ld ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  reg.(r0) <- mem_read vm ((reg.(r_pc) + pc_offset) land 0xffff);
+  update_flags vm r0
+
+let exec_ldi ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  reg.(r0) <- mem_read vm (mem_read vm ((reg.(r_pc) + pc_offset) land 0xffff));
+  update_flags vm r0
+
+let exec_ldr ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let r1 = (instr lsr 6) land 0x7 in
+  let offset = sign_extend (instr land 0x3f) 6 in
+  reg.(r0) <- mem_read vm ((reg.(r1) + offset) land 0xffff);
+  update_flags vm r0
+
+let exec_lea ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  reg.(r0) <- (reg.(r_pc) + pc_offset) land 0xffff;
+  update_flags vm r0
+
+let exec_st ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  mem_write vm ((reg.(r_pc) + pc_offset) land 0xffff) reg.(r0)
+
+let exec_sti ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let pc_offset = sign_extend (instr land 0x1ff) 9 in
+  mem_write vm (mem_read vm ((reg.(r_pc) + pc_offset) land 0xffff)) reg.(r0)
+
+let exec_str ({ reg; _ } as vm) instr =
+  let r0 = (instr lsr 9) land 0x7 in
+  let r1 = (instr lsr 6) land 0x7 in
+  let offset = sign_extend (instr land 0x3f) 6 in
+  mem_write vm ((reg.(r1) + offset) land 0xffff) reg.(r0)
+
+let exec_trap_getc ({ reg; _ } as vm) =
+  reg.(r_r0) <- input_char stdin |> int_of_char;
+  update_flags vm r_r0
+
+let exec_trap_out { reg; _ } =
+  output_char stdout (reg.(r_r0) |> char_of_int);
+  flush stdout
+
+let exec_trap_puts { memory; reg; _ } =
+  let rec aux i =
+    let c = memory.(reg.(r_r0) + i) in
+    if c = 0 then ()
+    else (
+      output_char stdout (char_of_int c);
+      aux (i + 1))
+  in
+  aux 0;
+  flush stdout
+
+let exec_trap_in ({ reg; _ } as vm) =
+  print_string "Enter a character: ";
+  flush stdout;
+  let c = input_char stdin in
+  output_char stdout c;
+  flush stdout;
+  reg.(r_r0) <- int_of_char c;
+  update_flags vm r_r0
+
+let exec_trap_putsp { memory; reg; _ } =
+  let rec aux i =
+    let c = memory.(reg.(r_r0) + i) in
+    if c = 0 then ()
+    else
+      let char1 = c land 0xff in
+      if char1 = 0 then ()
+      else (
+        output_char stdout (char_of_int char1);
+        let char2 = c lsr 8 in
+        if char2 = 0 then ()
+        else (
+          output_char stdout (char_of_int char2);
+          aux (i + 1)))
+  in
+  aux 0;
+  flush stdout
+
+let exec_trap_halt vm =
+  print_endline "HALT";
+  flush stdout;
+  vm.running <- false
+
+let exec_trap ({ reg; _ } as vm) instr =
+  reg.(r_r7) <- reg.(r_pc);
+  match instr land 0xff |> trapcode_of_int with
+  | Trap_getc -> exec_trap_getc vm
+  | Trap_out -> exec_trap_out vm
+  | Trap_puts -> exec_trap_puts vm
+  | Trap_in -> exec_trap_in vm
+  | Trap_putsp -> exec_trap_putsp vm
+  | Trap_halt -> exec_trap_halt vm
+
+let run ({ reg; running; _ } as vm) =
+  let rec aux () =
+    (if not running then ()
+    else
+      let pc = reg.(r_pc) in
+      reg.(r_pc) <- pc + 1;
+      let instr = mem_read vm pc in
+      match instr lsr 12 |> opcode_of_int with
+      | Op_add -> exec_add vm instr
+      | Op_and -> exec_and vm instr
+      | Op_not -> exec_not vm instr
+      | Op_br -> exec_br vm instr
+      | Op_jmp -> exec_jmp vm instr
+      | Op_jsr -> exec_jsr vm instr
+      | Op_ld -> exec_ld vm instr
+      | Op_ldi -> exec_ldi vm instr
+      | Op_ldr -> exec_ldr vm instr
+      | Op_lea -> exec_lea vm instr
+      | Op_st -> exec_st vm instr
+      | Op_sti -> exec_sti vm instr
+      | Op_str -> exec_str vm instr
+      | Op_trap -> exec_trap vm instr
+      | Op_rti -> failwith "RTI"
+      | Op_res -> failwith "RES");
+    aux ()
+  in
+  aux ()
+
+let load_arguments vm =
+  let argc = Array.length Sys.argv in
+  if argc < 2 then (
+    print_endline "lc3 [image-file1] ...";
+    exit 2);
+  for i = 1 to argc - 1 do
+    read_image vm Sys.argv.(i)
+  done
+
+let () =
+  let vm = make_vm () in
+  load_arguments vm;
   setup ();
-
-  (* Since exactly one condition flag should be set at any given time,
-     set the Z flag. *)
-  reg.(r_cond) <- fl_zro;
-
-  (* Set the PC to starting position. *)
-  (* 0x3000 is the default. *)
-  let pc_start = 0x3000 in
-  reg.(r_pc) <- pc_start;
-
-  run ()
-
-let () = main ()
+  run vm
